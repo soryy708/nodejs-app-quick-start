3091438f083c74c7b514b76b2d8a0737b1e6f380
Ivan Rubinson <soryy708@gmail.com>
2019-09-21 19:41:37 +0300

    Add Sass preprocessor

---
+++ scripts/build/front/css.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const sourceMaps = require('gulp-sourcemaps');
+
+const cssAssets = '../src/front/**/*.css';
+const sassAssets = '../src/front/**/*.scss';
+const sassOptions= {
+    outputStyle: 'compressed',
+};
+
+gulp.task('build-front/css/plain', () => {
+    return gulp.src(cssAssets)
+        .pipe(gulp.dest('../build/front'));
+});
+
+gulp.task('build-front/css/sass', () => {
+    return gulp.src(sassAssets)
+        .pipe(sourceMaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest('../build/front'));
+});
+
+gulp.task('build-front/css', gulp.series('build-front/css/plain', 'build-front/css/sass'));
+
+gulp.task('watch-front/css/plain', () => {
+    return gulp.watch(cssAssets, gulp.series('build-front/css/plain'));
+});
+
+gulp.task('watch-front/css/sass', () => {
+    return gulp.watch(sassAssets, gulp.series('build-front/css/sass'));
+});
+
+gulp.task('watch-front/css', gulp.parallel('watch-front/css/plain', 'watch-front/css/sass'));
--- scripts/build/front/index.js
+++ scripts/build/front/index.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 
+require('./css'); // eslint-disable-line import/no-unassigned-import
 require('./javascript'); // eslint-disable-line import/no-unassigned-import
 require('./others'); // eslint-disable-line import/no-unassigned-import
 
-gulp.task('build-front', gulp.parallel('build-front/javascript', 'build-front/others'));
-gulp.task('watch-front', gulp.parallel('watch-front/javascript', 'watch-front/others'));
+gulp.task('build-front', gulp.parallel('build-front/javascript', 'build-front/css', 'build-front/others'));
+gulp.task('watch-front', gulp.parallel('watch-front/javascript', 'watch-front/css', 'watch-front/others'));
 gulp.task('autobuild-front', gulp.parallel('build-front', 'watch-front'));
